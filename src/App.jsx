ff94b30a8149341d7be2b393803067cb0cc31122
Deepak-learn-web <152325375+Deepak-learn-web@users.noreply.github.com>
2025-12-23 00:00:48 +0530

    Implemented Hero Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/all";
+import Hero from "./sections/Hero";
+import NavBar from "./sections/NavBar";
 
 gsap.registerPlugin(ScrollTrigger);
 
 const App = () => {
   return (
     <main>
-      <div>
-        <h1 className="text-5xl text-pink ">
-          Welcome To The Project Of GTA & GSAP
-        </h1>
-      </div>
+      <NavBar />
+      <Hero />
     </main>
   );
 };
