f3d56c84addc25f7e3f2e46706d9e25e3faa727e
Deepak-learn-web <152325375+Deepak-learn-web@users.noreply.github.com>
2025-12-30 21:29:22 +0530

    Implemented PostCard

--- src/sections/Jason.jsx
+++ src/sections/Jason.jsx
@@ -3,9 +3,8 @@ import { useGSAP } from "@gsap/react";
 
 const Jason = () => {
   useGSAP(() => {
-    gsap.set(".jason", {
-      marginTop: "-80vh",
-    });
+    gsap.set(".jason", { marginTop: "-80vh" });
+
     gsap
       .timeline({
         scrollTrigger: {
@@ -33,6 +32,7 @@ const Jason = () => {
       "<"
     );
   });
+
   return (
     <section className="jason">
       <div className="max-w-lg jason-content">
@@ -44,16 +44,18 @@ const Jason = () => {
           doing what he knows best, working for local drug runners. It might be
           time to try something new.
         </p>
+
         <div className="jason-2">
-          <img src="/images/jason-2.webp" alt="" />
+          <img src="/images/jason-2.webp" />
         </div>
       </div>
+
       <div className="space-y-5 mt-96 img-box">
         <div className="jason-1">
-          <img src="/images/jason-1.webp" alt="" />
+          <img src="/images/jason-1.webp" />
         </div>
         <div className="jason-3">
-          <img src="/images/jason-3.webp" alt="" />
+          <img src="/images/jason-3.webp" />
         </div>
       </div>
     </section>
